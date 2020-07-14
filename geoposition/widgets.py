cc466dcb05231329a5e01e3a08567b1b0e313230
erickTornero <erickdeivy01@gmail.com>
2020-07-14 01:43:39 -0500

    [MOD] django.six library removed, instead <import six> is used

--- geoposition/widgets.py
+++ geoposition/widgets.py
@@ -4,7 +4,7 @@ import json
 
 from django import forms
 from django.template.loader import render_to_string
-from django.utils import six
+import six
 from django.utils.translation import ugettext_lazy as _
 
 from .conf import settings
